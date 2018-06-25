e1abe56ef539c5434d753a42dbec888a166819f4
unionsep <union.septem@gmail.com>
2018-06-25 19:20:17 +0900

    add option setting and http get begin and commit url

--- src/background.js
+++ src/background.js
@@ -1,13 +1,42 @@
-if (localStorage['icon'] === undefined) {
-	localStorage['icon'] = 'img/blue.png';
-}
-
-
-chrome.browserAction.onClicked.addListener(function(tab){
-	if (localStorage['icon'] === 'img/blue.png') {
-		localStorage['icon'] = 'img/red.png';
-	} else {
+$(function(){
+	if (localStorage['icon'] === undefined) {
 		localStorage['icon'] = 'img/blue.png';
+		chrome.browserAction.setIcon({path:localStorage['icon']});
 	}
-	chrome.browserAction.setIcon({path:localStorage['icon']});
-});	
+
+	chrome.browserAction.onClicked.addListener(function(tab){
+		if (localStorage['icon'] === 'img/blue.png') {
+			var url = getProxyUrl('begin');
+			$.get(url)
+			.done(function(data){
+				localStorage['icon'] = 'img/red.png';
+				chrome.browserAction.setIcon({path:localStorage['icon']});
+			})
+			.fail(function(data){
+				localStorage['icon'] = 'img/blue.png';
+				chrome.browserAction.setIcon({path:localStorage['icon']});
+			});
+		} else {
+			var url = getProxyUrl('commit');
+			$.get(url)
+			.done(function(data){
+				localStorage['icon'] = 'img/blue.png';
+				chrome.browserAction.setIcon({path:localStorage['icon']});
+			})
+			.fail(function(data){
+				localStorage['icon'] = 'img/red.png';
+				chrome.browserAction.setIcon({path:localStorage['icon']});
+			});
+		}
+	});
+
+	function getProxyUrl(operation) {
+		if (localStorage['vaddy-server-name'] !== undefined && localStorage['vaddy-verification-filename'] !== undefined) {
+			if ('begin' == operation) {
+				return localStorage['vaddy-server-name'] + '/' + localStorage['vaddy-verification-filename'] + '?action=begin';
+			} else if ('commit' == operation) {
+				return localStorage['vaddy-server-name'] + '/' + localStorage['vaddy-verification-filename'] + '?action=commit';
+			}
+		}
+	}
+});
--- view/options/options.js
+++ view/options/options.js
@@ -1,6 +1,19 @@
-function save_url() {
-   var url = document.getElementById('vaddy-crawl-url').value;
-   console.log(url);
-}
+window.onload = function(){
+    if (localStorage['vaddy-server-name'] !== undefined) {
+        document.getElementById('vaddy-server-name').value = localStorage['vaddy-server-name'];
+    }
+    
+    if (localStorage['vaddy-verification-filename'] !== undefined) {
+        document.getElementById('vaddy-verification-filename').value = localStorage['vaddy-verification-filename'];
+    }
 
-document.getElementById("save").addEventListener("click", save_url);
+    function saveSetting() {
+        var serverName = document.getElementById('vaddy-server-name').value;
+        var verificationFilename = document.getElementById('vaddy-verification-filename').value;
+        localStorage['vaddy-server-name'] = serverName;
+        localStorage['vaddy-verification-filename'] = verificationFilename;
+        window.close();
+     }
+     
+     document.getElementById("save-setting").addEventListener("click", saveSetting);
+}
