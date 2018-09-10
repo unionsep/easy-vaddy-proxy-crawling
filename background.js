ac6ef6cb3521b4e3bdee81928ed59b47c6b18302
unionsep <union.septem@gmail.com>
2018-09-10 19:56:59 +0900

    add project id to crowl url

--- background.js
+++ background.js
@@ -1,98 +1,135 @@
 $(() => {
-    const iconBlue = 'icons/blue_19.png';
-    const iconRed = 'icons/red_19.png';
+    const PROC_BEGIN = 'begin';
+    const PROC_COMMIT = 'commit';
+    const ICON_REC = 'icons/red_19.png';
+    const ICON_UNREC = 'icons/blue_19.png';
+    const VERSION_V1 = 1;
+    const VERSION_V2 = 2;
 
-    if (localStorage.getItem('icon') === undefined) {
-        setIconBlue();
+    let getBasicCredentials = () => {
+        var basicUser = localStorage.getItem('basic-user')
+        var basicPass = localStorage.getItem('basic-pass')
+        if (null != basicUser && null != basicPass) {
+            return window.btoa(basicUser + ':' + basicPass)
+        }
     }
 
-    browser.browserAction.onClicked.addListener(() => {
-        if (localStorage['icon'] == iconBlue) {
-            var urls = getProxyURL('begin');
-            if (0 >= urls.length) {
-                createNotification('BEGIN', 'Error', 'check Server Name or Verification Code');
-                return;
-            }
-            $.ajax({
-                url: url,
-                success: (data) => {
-                    if (data.trim() ==  localStorage['verify-code'].trim()) {
-                        createNotification('BEGIN', 'Success', url);
-                        if (1 == localStorage['auto-access']) {
-                            var scanUrl = getAutoAccessUrl();
-                            browser.tabs.create({
-                                url: browser.runtime.getURL(scanUrl)
-                            });
-                        }
-                        setIconRed();
-                    } else {
-                        createNotification('BEGIN', 'Failed', url);
-                        setIconBlue();
-                    }
-                },
-                error: (xhr, status, error) => {
-                    createNotification('BEGIN', 'Error', url);
-                    setIconBlue();
-                },
-                beforeSend: (xhr) => {
-                    var credentials = getBasicCredentials();
-                    if (credentials) {
-                        xhr.setRequestHeader('Authorization', 'Basic ' + credentials);
-                    }
-                }
-            });
-        } else {
-            var url = getProxyURL('commit');
-            $.ajax({
-                url: url,
-                success: (data) => {
-                    if (data.trim() == localStorage['verify-code'].trim()) {
-                        createNotification('COMMIT', 'Success', url);
-                        setIconBlue();
-                    } else {
-                        createNotification('COMMIT', 'Failed', url);
-                        setIconRed();
-                    }
-                },
-                error: (xhr, status, error) => {
-                    createNotification('COMMIT', 'Error', url);
-                    setIconRed();
-                },
-                beforeSend: (xhr) => {
-                    var credentials = getBasicCredentials();
-                    if (credentials) {
-                        xhr.setRequestHeader('Authorization', 'Basic ' + credentials);
-                    }
-                }
-            });
+    let getCrawlingURL = (operation) => {
+        if (PROC_BEGIN !== operation && PROC_COMMIT !== operation) {
+            return []
         }
-    });
+        var urls = []
+        var serverNames = localStorage.getItem('server-names')
+        var verifyCode = localStorage.getItem('verify-code')
+        var version = localStorage.getItem('version')
+        var projectId = localStorage.getItem('project-id')
+        if (serverNames !== undefined && verifyCode !== undefined) {
+            var serverNames = JSON.parse(serverNames)
+            for (let server in serverNames) {
+                if (null != serverNames[server]) {
+                    if (VERSION_V2 == version) {
+                        urls.push(serverNames[server][1].trim() + '/vaddy-' + verifyCode.trim() + '.html?action=' + operation + '&project_id=' + projectId)
+                    } else {
+                        urls.push(serverNames[server][1].trim() + '/vaddy-' + verifyCode.trim() + '.html?action=' + operation)
+                    }
+                }
+            }
+        }
+        return urls
+    }
 
-    let setIconRed = () => {
-        localStorage['icon'] = iconRed;
-        browser.browserAction.setIcon({path: iconRed});
-    };
-
-    let setIconBlue = () => {
-        localStorage['icon'] = iconBlue;
-        browser.browserAction.setIcon({path: iconBlue});
-    };
-
-    let createNotification = (proc, status, message) => {
-        var title = status + ' ' + proc + ' VAddy Proxy Crawl';
+    let setNotification = (proc, status, message) => {
         browser.notifications.create({
             'type': 'basic',
             'iconUrl': browser.extension.getURL('icons/blue_48.png'),
-            'title': title,
+            'title': browser.i18n.getMessage('notification_title', [status, proc]),
             'message': message
         });
     };
 
-    let getBasicCredentials = () => {
-        if (localStorage['basic-user'] !== undefined && localStorage['basic-pass'] !== undefined) {
-            return window.btoa(localStorage['basic-user'] + ':' + localStorage['basic-pass']);
+    let changeIcon = (proc) => {
+        if (PROC_BEGIN === proc) {
+            localStorage.setItem('icon', ICON_REC)
+            browser.browserAction.setIcon({path: ICON_REC})
+        } else {
+            localStorage.setItem('icon', ICON_UNREC)
+            browser.browserAction.setIcon({path: ICON_UNREC})
         }
     };
+
+    let accessCrawlURL = (proc, url) => {
+        var isAccessed = false;
+        $.ajax({
+            url: url,
+            async: false,
+            success: (data) => {
+                if (data.trim() == localStorage.getItem('verify-code')) {
+                    isAccessed = true;
+                } else {
+                    isAccessed = false
+                }
+            },
+            error: (xhr, status, error) => {
+                isAccessed = false
+            },
+            beforeSend: (xhr) => {
+                var credentials = getBasicCredentials();
+                if (credentials) {
+                    xhr.setRequestHeader('Authorization', 'Basic ' + credentials);
+                }
+            }
+        })
+        if (PROC_BEGIN == proc) {
+            isAccessed ? changeIcon(PROC_BEGIN) : changeIcon(PROC_COMMIT)
+        } else {
+            isAccessed ? changeIcon(PROC_COMMIT) : changeIcon(PROC_BEGIN)
+        }
+        return isAccessed
+    }
+
+    if (null == localStorage.getItem('icon')) {
+        changeIcon(PROC_COMMIT)
+    } else {
+        if (ICON_REC == localStorage.getItem('icon')) {
+            changeIcon(PROC_BEGIN)
+        } else {
+            changeIcon(PROC_COMMIT)
+        }
+    }
+
+    browser.browserAction.onClicked.addListener(() => {
+        if (ICON_UNREC === localStorage.getItem('icon')) {
+            var urls = getCrawlingURL(PROC_BEGIN)
+            console.log(urls)
+            if (0 >= urls.length) {
+                setNotification('BEGIN', 'Error', browser.i18n.getMessage('msg_failed_require_fqdn_verify_code'))
+            } else {
+                var isAccessed = true
+                $.each(urls, (idx, url) => {
+                    if (!accessCrawlURL(PROC_BEGIN, url)) {
+                        isAccessed = false
+                        return false
+                    }
+                })
+                isAccessed ? changeIcon(PROC_BEGIN) : changeIcon(PROC_COMMIT)
+            }
+        } else {
+            var urls = getCrawlingURL(PROC_COMMIT)
+            console.log(urls)
+            if (0 >= urls.length) {
+                setNotification('COMMIT', 'Error', browser.i18n.getMessage('msg_failed_require_fqdn_verify_code'))
+            } else {
+                var isAccessed = true
+                $.each(urls, (idx, url) => {
+                    if (!accessCrawlURL(PROC_COMMIT, url)) {
+                        isAccessed = false;
+                        return false;
+                    }
+                })
+                isAccessed ? changeIcon(PROC_COMMIT) : changeIcon(PROC_BEGIN);
+            }
+        }
+    });
 
     let getAutoAccessUrl = () => {
         var serverNames = localStorage.getItem('server-names');
@@ -108,21 +145,4 @@ $(() => {
         }
         return url;
     };
-
-    let getProxyURL = (operation) => {
-        var urls = [];
-        if (localStorage['server-names'] !== undefined && localStorage['verify-code'] !== undefined) {
-            var serverNames = JSON.parse(localStorage.getItem('server-names'));
-            for (let server in serverNames) {
-                if (null != serverNames[server]) {
-                    if ('begin' == operation) {
-                        urls.push(serverNames[server][1].trim() + '/vaddy-' + localStorage['verify-code'].trim() + '.html?action=begin');
-                    } else if ('commit' == operation) {
-                        urls.push(serverNames[server][1].trim() + '/vaddy-' + localStorage['verify-code'].trim() + '.html?action=commit');
-                    }
-                }
-            }
-        }
-        return urls;
-    }
-});
+})
--- options/options.js
+++ options/options.js
@@ -1,37 +1,51 @@
 $(() => {
+    const VERSION_V1 = 1;
+    const VERSION_V2 = 2;
+
     let setNotification = (proc, status, message) => {
         browser.notifications.create({
             'type': 'basic',
             'iconUrl': browser.extension.getURL('icons/blue_48.png'),
             'title': browser.i18n.getMessage('notification_title', [status, proc]),
             'message': message
-        });
-    };
+        })
+    }
 
     $(document).ready(event => {
-        var serverNames = localStorage.getItem('server-names');
-        var elemServerNames = $('.server-names');
+        var serverNames = localStorage.getItem('server-names')
+        var elemServerNames = $('.server-names')
         if  (null != serverNames) {
-            serverNames = JSON.parse(serverNames);
+            serverNames = JSON.parse(serverNames)
             for (let server in serverNames) {
                 if (null != serverNames[server]) {
-                    elemServerNames.eq(server).find('label input[type="radio"]').prop('checked', serverNames[server][0]);
-                    elemServerNames.eq(server).find('input[type="text"]').val(serverNames[server][1]);
+                    elemServerNames.eq(server).find('label input[type="radio"]').prop('checked', serverNames[server][0])
+                    elemServerNames.eq(server).find('input[type="text"]').val(serverNames[server][1])
                 } else {
-                    elemServerNames.eq(server).find('label input[type="radio"]').prop('checked', '');
-                    elemServerNames.eq(server).find('input[type="text"]').val('');
+                    elemServerNames.eq(server).find('label input[type="radio"]').prop('checked', '')
+                    elemServerNames.eq(server).find('input[type="text"]').val('')
                 }
             }
         }
         if (0 >= elemServerNames.find('label input[type="radio"]:checked').length) {
-            elemServerNames.eq(0).find('label input[type="radio"]').prop('checked', true);
+            elemServerNames.eq(0).find('label input[type="radio"]').prop('checked', true)
         }
-        $('#verify-code').val(localStorage.getItem('verify-code'));
-        $('#version').val(localStorage.getItem('version'));
-        $('#auto-access').prop('checked', localStorage.getItem('auto-access'));
-        $('#basic-user').val(localStorage.getItem('basic-user'));
-        $('#basic-pass').val(localStorage.getItem('basic-pass'));
-    });
+        $('#verify-code').val(localStorage.getItem('verify-code'))
+        var version = localStorage.getItem('version')
+        if (null == version) {
+            $('#version').val(VERSION_V2)
+        } else {
+            $('#version').val(version)
+        }
+        $('#project-id').val(localStorage.getItem('project-id'))
+        $('#auto-access').prop('checked', localStorage.getItem('auto-access'))
+        $('#basic-user').val(localStorage.getItem('basic-user'))
+        $('#basic-pass').val(localStorage.getItem('basic-pass'))
+    })
+
+    $('#version').on('change', event => {
+        var $this = $(event.currentTarget)
+        $this.val() == 2 ? $('#project-id').prop('disabled', false) : $('#project-id').prop('disabled', true)
+    })
 
     $('#save').on('click', event => {
         event.preventDefault();
@@ -54,51 +68,66 @@ $(() => {
             }
         });
 
-        var verifyCode = $('#verify-code').val();
+        var verifyCode = $('#verify-code').val()
+        var version = $('#version').val()
+        var projectID = $('#project-id').val()
         
         if ('' == verifyCode) {
-            setNotification('Option Save', 'Failed', browser.i18n.getMessage('msg_failed_require_verification'));
+            setNotification('Option Save', 'Failed', browser.i18n.getMessage('msg_failed_require_verification'))
         } else if (0 < invalidServerNum) {
-            setNotification('Option Save', 'Failed', browser.i18n.getMessage('msg_failed_invalid_fqdn_pattern', invalidServerNum));
+            setNotification('Option Save', 'Failed', browser.i18n.getMessage('msg_failed_invalid_fqdn_pattern', invalidServerNum))
         } else if (0 >= serverCount) {
-            setNotification('Option Save', 'Failed', browser.i18n.getMessage('msg_failed_require_fqdn'));
+            setNotification('Option Save', 'Failed', browser.i18n.getMessage('msg_failed_require_fqdn'))
+        } else if (2 == version && '' == projectID) {
+            setNotification('Option Save', 'Failed', browser.i18n.getMessage('msg_failed_version_project_id'))
         } else {
-            localStorage.setItem('server-names', JSON.stringify(serverNames));
-            localStorage.setItem('verify-code', verifyCode);
-            localStorage.setItem('version', $('#version').val());
+            localStorage.setItem('server-names', JSON.stringify(serverNames))
+            localStorage.setItem('verify-code', verifyCode)
+            localStorage.setItem('version', version)
+            if (VERSION_V2 == version) {
+                localStorage.setItem('project-id', projectID)
+            } else {
+                localStorage.removeItem('project-id')
+                $('#project-id').val('')
+            }
             if ($('#auto-access').prop('checked')) {
-                localStorage.setItem('auto-access', true);
+                localStorage.setItem('auto-access', true)
             } else {
-                localStorage.setItem('auto-access', false);
+                localStorage.setItem('auto-access', false)
             }
-            var basicUser = $('#basic-user').val();
-            var basicPass = $('#basic-pass').val();
+            var basicUser = $('#basic-user').val()
+            var basicPass = $('#basic-pass').val()
             if (!basicUser || !basicPass) {
-                localStorage.removeItem('basic-user');
-                localStorage.removeItem('basic-pass');
+                localStorage.removeItem('basic-user')
+                localStorage.removeItem('basic-pass')
             } else {
-                localStorage.setItem('basic-user', basicUser);
-                localStorage.setItem('basic-pass', basicPass);
+                localStorage.setItem('basic-user', basicUser)
+                localStorage.setItem('basic-pass', basicPass)
             }
-            setNotification('Option Save', 'Success', browser.i18n.getMessage('msg_success_save_option'));
+            setNotification('Option Save', 'Success', browser.i18n.getMessage('msg_success_save_option'))
         }
     });
 
     $('#reset').on('click', event => {
-        event.preventDefault();
-        localStorage.removeItem('server-names');
-        localStorage.removeItem('verify-code');
-        localStorage.removeItem('auto-access');
-        localStorage.removeItem('basic-user');
-        localStorage.removeItem('basic-pass');
+        event.preventDefault()
+        localStorage.removeItem('server-names')
+        localStorage.removeItem('verify-code')
+        localStorage.removeItem('auto-access')
+        localStorage.removeItem('version')
+        localStorage.removeItem('project-id')
+        localStorage.removeItem('basic-user')
+        localStorage.removeItem('basic-pass')
         $('.server-names').each((idx, elem) => {
-            $(elem).find('input[type="text"]').val('');
-            $(elem).find('label input[type="radio"]').prop('checked', false);
-        });
-        $('#verify-code').val('');
-        $('#auto-access').prop('checked', false);
-        $('#basic-user').val('');
-        $('#basic-pass').val('');
-        $('.server-names').eq(0).find('label input[type="radio"]').prop('checked', true);
+            $(elem).find('input[type="text"]').val('')
+            $(elem).find('label input[type="radio"]').prop('checked', false)
+        })
+        $('#verify-code').val('')
+        $('#version').val(VERSION_V2)
+        $('#project-id').val('')
+        $('#project-id').prop('disabled', false)
+        $('#auto-access').prop('checked', false)
+        $('#basic-user').val('')
+        $('#basic-pass').val('')
+        $('.server-names').eq(0).find('label input[type="radio"]').prop('checked', true)
     });
 });
